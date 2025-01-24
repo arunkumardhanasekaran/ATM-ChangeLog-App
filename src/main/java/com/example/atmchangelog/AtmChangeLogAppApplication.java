00499ad46dddfc623ad43c0c821a53fa578d8ae7
arunkumardhanasekaran <arunkumar1826@gmail.com>
2025-01-24 15:09:46 +0530

    Initial Commit

---
+++ src/main/java/com/example/atmchangelog/AtmChangeLogAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.atmchangelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmChangeLogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmChangeLogAppApplication.class, args);
+	}
+
+}
